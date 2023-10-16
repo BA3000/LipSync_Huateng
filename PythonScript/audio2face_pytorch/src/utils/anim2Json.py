2204e98e746e812cd103c205eb819b0b575d6fb9
BA3000 <2864281096@qq.com>
2023-10-17 00:59:39 +0800

    Create anim2Json.py
    添加转换脚本，将算法输出的脚本转换成unity能读取的json格式

---
+++ PythonScript/audio2face_pytorch/src/utils/anim2Json.py
@@ -0,0 +1,33 @@
+# -*-coding:utf-8 -*-
+
+"""
+File    : anim2Json.py
+Time    : 2023/10/17
+Author  : yanhuiwu
+"""
+
+import os
+import json
+
+### config
+INPUT_FILE_PATH = "data\\test_demo\\rjbwks6091"
+OUT_PATH = "data\\test_demo"
+
+def main():
+    inputFileName = os.path.basename(INPUT_FILE_PATH)
+    outContent = dict()
+    outContent["data"] = []
+    if os.path.exists(INPUT_FILE_PATH):
+        print("INFO: path valid, converting")
+        for line in open(INPUT_FILE_PATH):
+            outContent["data"].append(json.loads(line.split("#")[2]))
+    else:
+        print("ERR: PATH DOES NOT EXIST!")
+    res = json.dumps(outContent)
+    outFileName = inputFileName.split(".")[0] + ".json"
+    outFilePath = os.path.join(OUT_PATH, outFileName)
+    with open(outFilePath, 'w') as f:
+        f.write(res)
+
+if __name__ == "__main__":
+    main()
